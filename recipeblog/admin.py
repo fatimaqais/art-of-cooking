7e6a9dcb8eb549be43de3cf7bc4f39f3d8b2d822
fatimaqais <fatima_qais@hotmail.com>
2023-05-12 22:04:41 +0000

    Install summernote

--- recipeblog/admin.py
+++ recipeblog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Recipe, Comment
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Recipe)
+
+@admin.register(Recipe)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('ingredients', 'direction')
