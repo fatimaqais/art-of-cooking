505a67bf0eca78475d7778b6577ad3f8a86596b6
fatimaqais <fatima_qais@hotmail.com>
2023-05-12 22:43:30 +0000

    Add recipe and comment to filter and search

--- recipeblog/admin.py
+++ recipeblog/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Recipe)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'excerpt', 'author', 'created_on', 'status')
+    list_filter = ('status', 'created_on')
+    search_fields = ['title', 'category']
     summernote_fields = ('ingredients', 'direction')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
+    search_fields = ['name', 'body', 'created_on']
+    list_filter = ('approved', 'created_on')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
