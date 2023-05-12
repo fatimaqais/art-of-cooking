3a3015aca928a9b6b447484359a9403f1b8fc161
fatimaqais <fatima_qais@hotmail.com>
2023-05-12 21:11:52 +0000

    Create database models and make migrations

--- recipeblog/models.py
+++ recipeblog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    image_1 = CloudinaryField('image')
+    image_2 = CloudinaryField('image')
+    excerpt = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted_recipes')
+    category = models.TextField()
+    servings = models.PositiveIntegerField()
+    ingredients = models.TextField()
+    direction = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
