c9d2e191db65aa5eb586765de096df09a14b5fd7
fatimaqais <fatima_qais@hotmail.com>
2023-05-15 22:24:29 +0000

    Add url to view the selected recipe details

--- recipeblog/urls.py
+++ recipeblog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostRecipe.as_view(), name='home')
+    path('', views.PostRecipe.as_view(), name='home'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='view_recipe'),
 ]
--- recipeblog/views.py
+++ recipeblog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,24 @@ class PostRecipe(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "view_recipe.html",
+            {
+                "recipe": post,
+                "comments": comment,
+                "liked": liked
+            },
+        )
