0ed1a0055f9fc0eb4b37d803334d1a650482c5c1
fatimaqais <fatima_qais@hotmail.com>
2023-05-16 18:24:56 +0000

    Add function to post comment and wait for approval

--- recipeblog/views.py
+++ recipeblog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Recipe
+from .forms import CommentForm
 
 
 class PostRecipe(generic.ListView):
@@ -26,6 +28,45 @@ class RecipeDetail(View):
             {
                 "recipe": post,
                 "comments": comment,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        """
+        Post method for comment form setting recipe object and returning
+        variables.
+        """
+
+        queryset = Recipe.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "view_recipe.html",
+            {
+                "recipe": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
