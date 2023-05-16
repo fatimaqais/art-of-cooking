6dde59d493a21a273f77605c868f2461cbe88ec8
fatimaqais <fatima_qais@hotmail.com>
2023-05-16 19:25:07 +0000

    Add urls path for liked recipe

--- recipeblog/urls.py
+++ recipeblog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostRecipe.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='view_recipe'),
+    path('like/<slug:slug>', views.LikeRecipe.as_view(), name='like_recipe'),
 ]
