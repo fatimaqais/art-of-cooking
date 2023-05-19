78e8137a11db7d5a7b22d372db508b56590080bc
fatimaqais <fatima_qais@hotmail.com>
2023-05-19 18:34:14 +0000

    Add url for share_recipe page

--- recipeblog/urls.py
+++ recipeblog/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostRecipe.as_view(), name='home'),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name='view_recipe'),
+    path('view_recipe/<slug:slug>/', views.RecipeDetail.as_view(),
+         name='view_recipe'),
     path('like/<slug:slug>', views.LikeRecipe.as_view(), name='like_recipe'),
+    path('share_recipe/', views.ShareRecipe.as_view(), name='share_recipe'),
 ]
