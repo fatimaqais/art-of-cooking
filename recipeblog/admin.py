5098fbb66232e0f4954dc9e9b6ec2cb8e609a129
fatimaqais <fatima_qais@hotmail.com>
2023-05-19 18:33:07 +0000

    Change how the slug field is populated

--- recipeblog/admin.py
+++ recipeblog/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title', 'author', 'category',)}
     list_display = ('title', 'excerpt', 'author', 'created_on', 'status')
     list_filter = ('status', 'created_on')
     search_fields = ['title', 'category']
