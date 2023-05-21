a93fc65b0da77f27fbd02ce9d6b9e7427557c9ea
fatimaqais <fatima_qais@hotmail.com>
2023-05-21 21:58:05 +0000

    Add recipe form to create a new post

--- recipeblog/forms.py
+++ recipeblog/forms.py
@@ -1,8 +1,39 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms
+from django.forms import ModelForm, TextInput
+from django_summernote.widgets import SummernoteWidget
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+
+    class Meta:
+        model = Recipe
+
+        widgets = {
+            'direction': SummernoteWidget(),
+            'ingredients': SummernoteWidget(),
+            'excerpt': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Enter a short description of your recipe'
+                }),
+        }
+
+        fields = [
+            'title',
+            'excerpt',
+            'author',
+            'featured_image',
+            'image_1',
+            'category',
+            'servings',
+            'ingredients',
+            'direction',
+            'status',
+        ]
