b25838b3e78c241969d287a980e27145e40b1247
fatimaqais <fatima_qais@hotmail.com>
2023-05-22 19:57:08 +0000

    Add view recipes created by user and create new recipe view class

--- recipeblog/views.py
+++ recipeblog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
+from django.views.generic import ListView, View, CreateView, UpdateView
 from django.http import HttpResponseRedirect
-from .models import Recipe
-from .forms import CommentForm
+from .models import Recipe, Comment
+from .forms import CommentForm, RecipeForm
 
 
-class PostRecipe(generic.ListView):
+class PostRecipe(ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -19,6 +19,7 @@ class RecipeDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comment = post.comments.filter(approved=True).order_by('created_on')
         liked = False
+        paginate_by = 4
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
@@ -30,7 +31,7 @@ class RecipeDetail(View):
                 "comments": comment,
                 "commented": False,
                 "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(),
             },
         )
 
@@ -67,7 +68,7 @@ class RecipeDetail(View):
                 "comments": comments,
                 "commented": True,
                 "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(),
             },
         )
 
@@ -82,3 +83,35 @@ class LikeRecipe(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('view_recipe', args=[slug]))
+
+
+class ShareRecipe(CreateView):
+
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'share_recipe.html'
+
+
+class UserRecipes(ListView):
+
+    model = Recipe
+    template_name = 'user_recipes.html'
+
+    def get(self, request):
+
+        queryset = Recipe.objects.filter(
+            author=request.user.id).order_by('-created_on')
+        queryset_dict = {
+            'your_recipes': queryset,
+        }
+
+        return render(
+            request,
+            self.template_name,
+            queryset_dict,
+        )
+
+
+class EditRecipe(UpdateView):
+    model = Recipe
+    template_name = 'edit_recipe.html'
